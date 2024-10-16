5418f46f2ba28f51614b6aa43e8a2f3f45ca5ca6
rohitwebdeveloper <you@example.com>
2024-10-16 20:26:27 +0530

    Updated apiconfig file

--- src/api/apiConfig.jsx
+++ src/api/apiConfig.jsx
@@ -1,5 +1,6 @@
 // const apiurl = 'http://localhost:8000'
-// const apiurl = 'https://estore-backend-de6f.onrender.com';
-const apiurl = import.meta.env.BACKEND_SERVER_URL;
+// const apiurl = import.meta.env.BACKEND_SERVER_URL;
+
+const apiurl = 'https://estore-backend-de6f.onrender.com';
 
 export default apiurl;
